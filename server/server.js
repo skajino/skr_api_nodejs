cf8204b87fc94fd4189d00426271fef5ea7fd922
Selma Krajinovic <skrajino@bih.net.ba>
2018-08-10 08:40:50 +0200

    'Heroku'

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ app.get('/todos/:id', (req,res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
