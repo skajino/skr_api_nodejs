7b91702849472e2dd4f1495e54ec4f43cbf18704
Selma Krajinovic <skrajino@bih.net.ba>
2018-08-10 09:26:09 +0200

    mod heroku

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
 
 var app = express();
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -55,7 +55,6 @@ app.get('/todos/:id', (req,res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
-
 module.exports = {app};
