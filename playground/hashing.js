4f76ba74ee7729c10e141d3ee8c181730928fa86
Selma Krajinovic <skrajino@bih.net.ba>
2018-08-13 11:35:36 +0200

    Auth

---
+++ playground/hashing.js
@@ -0,0 +1,30 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+  id:10
+};
+
+var token = jwt.sign(data,'123aaaa');
+
+console.log(token);
+
+var decoded =jwt.verify(token+1,'123aaaa');
+
+console.log('decoded ', decoded);
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message ${message}`);
+// console.log(`hash ${hash}`);
+//
+// var data = {
+//   id: 4
+// };
+//
+//
+// var token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somescrtet').toString()
+// }
+// var resultHash = SHA256(JSON.stringify(token.data)+ 'somescrtet').toString();
