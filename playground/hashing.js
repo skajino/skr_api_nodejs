9da5d35ff60833fb5ae814481ed2be18001da703
Selma Krajinovic <skrajino@bih.net.ba>
2018-08-13 13:05:48 +0200

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
-var data = {
-  id:10
-};
+const bcrypt = require('bcryptjs');
 
-var token = jwt.sign(data,'123aaaa');
+var password = '123abc!';
 
-console.log(token);
+bcrypt.genSalt(10, (err,salt) =>{
+  bcrypt.hash(password, salt, (err,hash) =>{
+    console.log(hash);
+  })
+})
 
-var decoded =jwt.verify(token+1,'123aaaa');
+var hashedPassword = '$2a$10$rWrfO7.bTl6mlFsubqiEk.JTiX8ou0K4vr4qJ1zGmIY5sT2UCZzXq';
 
-console.log('decoded ', decoded);
-// var message = 'I am user number 3';
+bcrypt.compare(password, hashedPassword, (err,res) =>{
+  console.log(res);
+})
+
+// var data = {
+//   id:10
+// };
+//
+// var token = jwt.sign(data,'123aaaa');
+//
+// console.log(token);
+//
+// var decoded =jwt.verify(token,'123aaaa');
+//
+// console.log('decoded ', decoded);
+// // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
 // console.log(`Message ${message}`);
